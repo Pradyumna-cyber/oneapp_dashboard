54147739b1bb8a9251f3f4a261556fe46f574d9a
Pradyumna-cyber <73057121+Pradyumna-cyber@users.noreply.github.com>
2024-05-23 21:53:34 +0530

    Initial Commit

---
+++ oneapp_dashboard/oneapp_dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneapp_dashboardApp.swift
+//  oneapp_dashboard
+//
+//  Created by Pradyumna Deepak Aher on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct oneapp_dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
